fee84936326a8c46b5ac082a8b36cc2fc362e8bf
sehn-one <isehn1219@gmail.com>
2022-05-17 14:54:09 +0900

    Tab navigation 추가 및 에러 수정

--- App.js
+++ App.js
@@ -5,16 +5,16 @@ import * as Font from "expo-font"
 import { Ionicons } from "@expo/vector-icons" 
 import { Asset, useAssets } from "expo-asset"
 import { Image } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/Tabs';
 
 export default function App() {
 
   const [assets] = useAssets( [require("./assets/test.png"), "https://s.pstatic.net/static/www/img/uit/sp_main_dba1af.png" ])
   const [loaded] = Font.useFonts(Ionicons.font);
- 
-  if(!assets || !loaded){
-    return <AppLoading />;
-  }
-  
-  return null;
+
+  return <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  ;
 }
