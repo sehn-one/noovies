bf49046545ce81d4fda411686fe0685a28bc0586
sehn-one <isehn1219@gmail.com>
2022-05-20 15:53:22 +0900

    영화설명 섹션 추가

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -29,7 +29,39 @@ const BgImg = styled.Image`
     position:absolute;
 `
 
-const Title = styled.Text``
+const Title = styled.Text`
+    font-size: 16px;
+    font-weight: 600;
+    color: white;
+`
+
+const Poster = styled.Image`
+    width: 100px;
+    height: 160px;
+    border-radius: 5px;
+`
+
+const Wrapper = styled.View`
+    flex-direction:row;
+    height: 100%;
+    justify-content: center;
+    align-items:center;
+`
+
+const Column = styled.View`
+    width: 40%;
+    margin-left: 5px;
+
+`
+const Overview = styled.Text`
+    margin-top: 10px;
+    color: rgba(255,255,255,0.6)
+`
+
+const Votes = styled(Overview)`
+    margin-top: 5px;
+    font-size: 12px;
+`
 
 const { height: SCREEN_HEIGHT } =  Dimensions.get("window")  
 
@@ -55,8 +87,19 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
                 {nowPlaying.map( (movie) =>
                     (<View key={movie.id} >
                         <BgImg source={{ uri: makeImgPath(movie.backdrop_path) }} /> 
-                        <BlurView tint={isDark?"dark":"light"} style={StyleSheet.absoluteFill} intensity={20} >
-                            <Title>{movie.original_title}</Title>
+                        <BlurView tint={isDark? "dark" : "light" } style={StyleSheet.absoluteFill} intensity={10} >
+                          
+                            <Wrapper>
+                                <Poster source={{ uri: makeImgPath(movie.poster_path) }}/>
+                                <Column>
+                                    <Title>{movie.original_title}</Title>
+                                    {movie.vote_average > 0 ? <Votes>⭐️ {movie.vote_average}/10</Votes> : null}
+                                    <Overview>{movie.overview.slice(0,80)+"..."}</Overview>
+                                  
+                                    
+                                </Column>
+                            </Wrapper>
+                           
                         </BlurView>
                     </View>) )}            
             </Swiper>
